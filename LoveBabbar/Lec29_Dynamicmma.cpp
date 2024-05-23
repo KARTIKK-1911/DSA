b09c916404695ad97ea9f94530014066dffd1cb2
KARTIKK-1911 <kartikbansal1911@gmail.com>
2024-05-23 23:28:36 +0530

    dynamic

--- LoveBabbar/Lec29_Dynamicmma.cpp
+++ LoveBabbar/Lec29_Dynamicmma.cpp
@@ -1,6 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void main(){
-    cout<<"Jello";
+int main(){
+    int row, col;
+    cout<<"Enter row"<<endl;
+    cin>>row;
+    cout<<"Enter Column"<<endl;
+    cin>>col;
+
+    int** arr =new int*[row];
+
+    for(int i=0;i<row;i++){
+        arr[i]= new int[col];
+        
+    }
+    cout<<"Enter values in 2d array"<<endl;
+     for(int i=0;i<row;i++){
+        for(int j=0;j<col;j++){
+            cin>>arr[i][j];
+        }
+    }
+
+    cout<<"Entered values are"<<endl;
+     for(int i=0;i<row;i++){
+        for(int j=0;j<col;j++){
+            cout<<arr[i][j]<<" ";
+        }cout<<endl; 
+    }
+
+
+return 0;
 }
