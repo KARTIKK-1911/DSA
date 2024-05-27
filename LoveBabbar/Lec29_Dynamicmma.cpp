fb6508e97762419c46ff98b40ce1419a525555a6
KARTIKK-1911 <kartikbansal1911@gmail.com>
2024-05-27 23:42:34 +0530

    DMA

--- LoveBabbar/Lec29_Dynamicmma.cpp
+++ LoveBabbar/Lec29_Dynamicmma.cpp
@@ -26,9 +26,13 @@ int main(){
         for(int j=0;j<col;j++){
             cout<<arr[i][j]<<" ";
         }cout<<endl; 
-        }cout<<endl; 
     }
 
+    for(int i=0;i<row;i++){
+        delete [] arr[row];
+    }
+
+    delete [] arr;
 
 return 0;
 }
